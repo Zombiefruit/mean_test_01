a3b6375b46fd78bd7b25f9aabcc9ed1deb971fe9
Kieran Williams <kieranmatthewwilliams@hotmail.com>
2016-07-11 17:20:00 +0300

    First commit.

---
+++ app.js
@@ -0,0 +1,15 @@
+var app = angular.module('flapperNews', []);
+
+app.controller('MainCtrl', [
+  '$scope',
+  function($scope) {
+    $scope.test = "Hello World";
+    $scope.posts = [
+      'post 1',
+      'post 2',
+      'post 3',
+      'post 4',
+      'post 5'
+    ];
+  }
+]);
